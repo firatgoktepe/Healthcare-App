8f3d060c0333b01ce74da55128076419478a24d4
firatgoktepe <firatgoktepe1986@gmail.com>
2024-08-07 18:42:40 +0300

    feat: fix some build issues

--- app/sentry-example-page/page.tsx
+++ app/sentry-example-page/page.tsx
@@ -68,7 +68,7 @@ export default function Page() {
 
         <p>
           Next, look for the error on the{" "}
-          <a href="https://javascript-mastery.sentry.io/issues/?project=4507458386526208">
+          <a href="https://libre-consulting.sentry.io/issues/?project=4504576321191936">
             Issues Page
           </a>
           .
--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: "javascript-mastery",
+  org: "libre-consulting",
   project: "care-pulse",
 
   // Only print logs for uploading source maps in CI
