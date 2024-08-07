2eeed780b4983b353021f9733f0d2bde397cb896
firatgoktepe <firatgoktepe1986@gmail.com>
2024-08-07 18:49:36 +0300

    feat: fix more build issues

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ export default withSentryConfig(nextConfig, {
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
   org: "libre-consulting",
-  project: "care-pulse",
+  project: "javascript-nextjs",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
